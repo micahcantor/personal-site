57d456d8ab2404497376a53cc4b0d211a105cb23
Micah Cantor <micahcantor01@gmail.com>
2021-01-18 18:25:10 -0500

    inline md images

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,7 +121,10 @@ module.exports = {
               // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
               strict: `ignore`
             }
-          }
+          },
+          {
+            resolve: `gatsby-remark-images`,
+          },
         ],
       },
       
